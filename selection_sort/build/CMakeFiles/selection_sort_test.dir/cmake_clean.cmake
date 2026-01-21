8b9462b920012c50fd4fa9edb40e86d3ab6ecd25
kyletsubaki <91851573+kyletsubaki@users.noreply.github.com>
2026-01-20 16:06:30 -0800

    init

---
+++ selection_sort/build/CMakeFiles/selection_sort_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/selection_sort_test.dir/selection_sort_test.cpp.o"
+  "CMakeFiles/selection_sort_test.dir/selection_sort_test.cpp.o.d"
+  "selection_sort_test"
+  "selection_sort_test.pdb"
+  "selection_sort_test[1]_tests.cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/selection_sort_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ selection_sort/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/kyletsubaki/Documents/ECS36C/selection_sort
+# Build directory: /Users/kyletsubaki/Documents/ECS36C/selection_sort/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/Users/kyletsubaki/Documents/ECS36C/selection_sort/build/selection_sort_test[1]_include.cmake")
+subdirs("_deps/googletest-build")
